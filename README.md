bab0c211e9ce55af505dc71ccf711503957cb9c3
Konstantin Schall <ko.schall@gmail.com>
2023-06-01 19:02:16 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Image-Encoders-for-GP-NNS
-This is the official reposotory for the ICMR23 paper "Improving Image Encoders for General-Purpose Nearest Neighbor Search and Classification"
+This is the official reposotory for the ICMR23 paper [Improving Image Encoders for General-Purpose Nearest Neighbor Search and Classification](https://doi.org/10.1145/3591106.3592266)
+
+This paper investigates the effectiveness
+of different vision foundation models on two challenging nearest
+neighbor search-based tasks: zero-shot retrieval and k-NN classification. A benchmark for evaluating the performance of various
+vision encoders and their pre-training methods is established, where
+significant differences in the performance of these models are ob-
+served. Additionally, we propose a fine-tuning regime that improves
+zero-shot retrieval and k-NN classification through training with a
+combination of large publicly available datasets without specializing in any data domain. Our results show that the retrained vision
+encoders have a higher degree of generalization across different
+search-based tasks and can be used as general-purpose embedding
+models for image retrieval.
+
+![General-purpose finetuning improves retrieval results](./images/results_table.JPG)
+
+# Environment
+`pip install -r requirements.txt`
+
+# Model Checkpoints
+| Base Model        | GFLOPs/image | Finetuning Loss | Average Benchmark Score | Link |
+|-------------------|--------------|-----------------|-------------------------|------|
+| CLIP ViT-B/16@224 | ~20          | ArcMargin       | 67.9                    | [checkpoint](https://visual-computing.com/files/GPNNS/GPNNS_ViTB.pth)     |
+| CLIP ViT-L/14@336 | ~180         | ArcMargin       | 77.3                    |   [checkpoint](https://visual-computing.com/files/GPNNS/GPNNS_ViTL.pth)   |
+
+# Inference
+An example on how to use our finetuned models for inference is shown in this [notebook](./notebooks/General-Purpose-Embedding%20ViT.ipynb)
+
+# Evaluation
+Coming Soon
+
+# Finetuning
+Coming Soon
---
+++ src/models/ViT_Model.py
@@ -0,0 +1,134 @@
+from collections import OrderedDict
+import torch
+from torch import nn
+import torch.utils.checkpoint as checkpoint
+
+"""
+Code is based on the OpenAI CLIP repository: https://github.com/openai/CLIP/blob/main/clip/model.py
+"""
+
+class QuickGELU(nn.Module):
+    def forward(self, x: torch.Tensor):
+        return x * torch.sigmoid(1.702 * x)
+
+
+class ResidualAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, n_head: int, attn_mask: torch.Tensor = None):
+        super().__init__()
+
+        self.attn = nn.MultiheadAttention(d_model, n_head)
+        self.ln_1 = nn.LayerNorm(d_model)
+        self.mlp = nn.Sequential(OrderedDict([
+            ("c_fc", nn.Linear(d_model, d_model * 4)),
+            ("gelu", QuickGELU()),
+            ("c_proj", nn.Linear(d_model * 4, d_model))
+        ]))
+        self.ln_2 = nn.LayerNorm(d_model)
+        self.attn_mask = attn_mask
+
+    def attention(self, x: torch.Tensor):
+        #self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
+        return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
+
+ 
+    def forward(self, x: torch.Tensor):
+        x = x + self.attention(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, width: int, layers: int, heads: int, attn_mask: torch.Tensor = None, use_checkpointing: bool = False):
+        super().__init__()
+        self.width = width
+        self.layers = layers
+        self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask) for _ in range(layers)])
+        self.use_checkpointing = use_checkpointing
+    def forward(self, x: torch.Tensor):
+        return self.resblocks(x)
+
+
+class VisionTransformer(nn.Module):
+    def __init__(self, input_resolution: int, patch_size: int, width: int, layers: int, heads: int, output_dim: int, use_checkpointing: bool = False):
+        super().__init__()
+        self.input_resolution = input_resolution
+        self.output_dim = output_dim
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=width, kernel_size=patch_size, stride=patch_size, bias=False)
+
+        scale = width ** -0.5
+        self.class_embedding = nn.Parameter(scale * torch.randn(width))
+        self.positional_embedding = nn.Parameter(scale * torch.randn((input_resolution // patch_size) ** 2 + 1, width))
+        self.ln_pre = nn.LayerNorm(width)
+
+        self.transformer = Transformer(width, layers, heads, use_checkpointing=use_checkpointing)
+
+        self.ln_post = nn.LayerNorm(width)
+        self.pre_embed_dim = width
+        self.proj = nn.Parameter(scale * torch.randn(width, output_dim))
+
+    def forward(self, x: torch.Tensor):
+        x = self.conv1(x)  # shape = [*, width, grid, grid]
+        x = x.reshape(x.shape[0], x.shape[1], -1)  # shape = [*, width, grid ** 2]
+        x = x.permute(0, 2, 1)  # shape = [*, grid ** 2, width]
+        x = torch.cat([self.class_embedding.to(x.dtype) + torch.zeros(x.shape[0], 1, x.shape[-1], dtype=x.dtype, device=x.device), x], dim=1)  # shape = [*, grid ** 2 + 1, width]
+        x = x + self.positional_embedding.to(x.dtype)
+        x = self.ln_pre(x)
+
+        x = x.permute(1, 0, 2)  # NLD -> LND
+        x = self.transformer(x)
+        x = x.permute(1, 0, 2)  # LND -> NLD
+
+        x = self.ln_post(x[:, 0, :])
+
+        if self.proj is not None:
+            x = x @ self.proj
+
+        return x
+
+class EmbeddingModel(torch.nn.Module):
+    
+    def __init__(self, backbone, use_proj=True):
+        super().__init__()
+        self.backbone = backbone
+        self.emb_dim = backbone.output_dim
+        if not use_proj:
+            self.backbone.proj = None
+            self.emb_dim = backbone.pre_embed_dim
+
+    
+    def forward(self, x):
+        
+        fv = self.backbone(x)
+        return fv
+
+def create_model(model_name="ViT-L", check_pth=None):
+
+    if model_name == "ViT-L":
+        vit_model = VisionTransformer(
+                    input_resolution=336 ,
+                    patch_size=14 ,
+                    width=1024 ,
+                    layers=24 ,
+                    heads=16 ,
+                    output_dim=768
+                )
+    elif model_name == "ViT-B":
+        vit_model = VisionTransformer(
+                    input_resolution=224,
+                    patch_size=16,
+                    width=768,
+                    layers=12,
+                    heads=12,
+                    output_dim=512
+                    )
+
+    model = EmbeddingModel(vit_model, False)
+    if check_pth is not None:
+        model.load_state_dict(torch.load(check_pth))
+
+    model.half()
+    model.backbone.half()
+    model.eval()
+    return model
+
+     
